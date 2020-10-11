6e0ac91cc26ca7179aada95542ce5d3bbe885710
Ken Angelo Bauzon <kenangelobauzon@gmail.com>
2020-10-12 01:22:32 +0800

    Splash

---
+++ app/src/main/java/com/moke/saneme/Common/SplashScreen.java
@@ -0,0 +1,57 @@
+package com.moke.saneme.Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.moke.saneme.R;
+import com.moke.saneme.User.UserDashboard;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+
+    //Variables
+    ImageView backgroundImage;
+    TextView PoweredByLine;
+
+    //Animations
+    Animation sideAnim, bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        //Hooks
+        backgroundImage = findViewById(R.id.background_image);
+        PoweredByLine = findViewById(R.id.powered_by_line);
+
+        //Animations
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        //set Animations on elements
+        backgroundImage.setAnimation(sideAnim);
+        PoweredByLine.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), UserDashboard.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, SPLASH_TIMER);
+    }
+}
